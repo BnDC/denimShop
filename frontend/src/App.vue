f180fc9686c16747cfa1a91381dfba57a8c72fbe
bufflect <bndgc@naver.com>
2022-12-04 11:32:44 +0900

    feat : vue template 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,23 @@
 <template>
+  <Header/>
+  <!-- Modal -->
+  <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog"
+       aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="w-100 pt-1 mb-5 text-right">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="" method="get" class="modal-content modal-body border-0 p-0">
+        <div class="input-group mb-2">
+          <input type="text" class="form-control" id="inputModalSearch" name="q" placeholder="Search ...">
+          <button type="submit" class="input-group-text bg-success text-light">
+            <i class="fa fa-fw fa-search text-white"></i>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <RouterView/>
 </template>
 
 <script>
@@ -9,6 +28,11 @@ export default {
 </script>
 
 <style>
+@import '../src/assets/css/fontawesome.css';
+@import '../src/assets/css/slick.min.css';
+@import '../src/assets/css/slick-theme.css';
+@import '../src/assets/css/templatemo.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
