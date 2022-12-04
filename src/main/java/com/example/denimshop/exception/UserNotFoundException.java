df408894dab137b8e154a9a54d92aebf96996b2a
bufflect <bndgc@naver.com>
2022-12-05 03:30:16 +0900

    rename : 메시지 수정

--- src/main/java/com/example/denimshop/exception/UserNotFoundException.java
+++ src/main/java/com/example/denimshop/exception/UserNotFoundException.java
@@ -4,6 +4,6 @@ package com.example.denimshop.exception;
 public class UserNotFoundException extends RuntimeException {
     @Override
     public String getMessage() {
-        return "존재하지 않습니다.";
+        return "존재하지 않는 회원 입니다.";
     }
 }
